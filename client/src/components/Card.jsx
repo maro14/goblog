eef9a046ccc297658aaa50ea2948f5039fe48590
Teheskhiel Fritz <teheskhielfritz@gmail.com>
2023-06-14 19:29:33 +0700

    added home

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+export const Card = ({id, title, author, category}) => {
+  return (
+    <div className='card'>
+        <Link to={`/${id}`}>
+            <h2>{title}</h2>
+        </Link>
+        <h3>{author}</h3>
+        <h3>{category}</h3>
+    </div>
+  )
+}
