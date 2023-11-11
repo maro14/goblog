4665bdb255eca39cc152b267f98845893d71780c
Teheskhiel Fritz <teheskhielfritz@gmail.com>
2023-11-11 19:06:28 +0700

    add card function

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,9 +3,15 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
     return (
     <nav className='navbar'>
-        <Link to="/">Home</Link>
-        <Link to="/search">Search</Link>
-        <Link to="/add">Post</Link>
+        <div className='navbar-logo'>
+            <h1>Go Blog</h1>
+        </div>
+        <div className='navbar-link'>
+            <Link to="/">Home</Link>
+            <Link to="/search">Search</Link>
+            <Link to="/add">Post</Link>
+        </div>
+
     </nav>
     )
 }
